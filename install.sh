752e222ac646ef791c7604d93b1fdaca8a354efc
nurre <nurre13@gmail.com>
2017-06-13 17:00:53 +0200

    Fixed error in script.

--- install.sh
+++ install.sh
@@ -8,26 +8,25 @@ function ask_yes_or_no() {
     esac
 }
 
-if ["no" == $(ask_yes_or_no "Have you ran 'sudo apt-get update' and 'sudo apt-get upgrade'? [Y/n]")]
+if [ "no" == $(ask_yes_or_no "Have you ran 'sudo apt-get update' and 'sudo apt-get upgrade'? [Y/n]") ]
    
 then
     echo "Do that before running the installation."
     exit 0
+
+else
+    echo "Starting installation of program and the packages needed."
+    cd ~
+
+    echo "Getting required packages."
+    sudo apt-get install python3-pyqt5
+    sudo pip3 install matplotlib
+    sudo pip3 install pymysql
+    sudo pip3 install pyserial
+
+    echo "mysql-server will now be installed, you will be prompted to enter a password for the root user."
+    sleep 5
+    sudo apt-get -y install mysql-server --fix-missing
+
+    find ~ -name PiMakers.desktop | xargs -I '{}' cp {} ~/Desktop
 fi
-
-echo "Starting installation of program and the packages needed."
-    
-cd ~
-
-echo "Getting required packages."
-sudo apt-get install python3-pyqt5
-sudo pip3 install matplotlib
-sudo pip3 install pymysql
-sudo pip3 install pyserial
-
-echo "mysql-server will now be installed, you will be prompted to enter a password for the root user."
-sleep 5
-sudo apt-get -y install mysql-server --fix-missing
-
-find ~ -name PiMakers.desktop | xargs -I '{}' cp {} ~/Desktop
-
